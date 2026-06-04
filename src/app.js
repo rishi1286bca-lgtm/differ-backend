99d9ce4335748d031c6d31e2238cf3bc4486806e
rishi1286bca-lgtm <rishi1286bca@gmail.com>
2026-06-04 14:10:16 +0530

    2

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const connectDB = require('./db/db');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://profound-vacherin-d01521.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
